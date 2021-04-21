adace84b88353e08d0213fdcf823c1b251b3fc34
Tokuhiro Matsuno <tokuhirom@gmail.com>
2021-04-21 11:27:48 +0900

    Show alerts

--- main.ts
+++ main.ts
@@ -63,7 +63,7 @@ export default class StopwatchPlugin extends Plugin {
 		});
 
 		this.registerView(VIEW_TYPE_STOPWATCH, (leaf: WorkspaceLeaf) => {
-			this.view = new StopWatchView(leaf, this, new StopwatchModel());
+			this.view = new StopWatchView(leaf, this);
 			return this.view;
 		});
 
@@ -93,6 +93,8 @@ export default class StopwatchPlugin extends Plugin {
 	}
 }
 
+const SETTING_INTERVAL_DESC = 'Valid value range is 1~1000'
+
 class StopwatchSettingTab extends PluginSettingTab {
 	plugin: StopwatchPlugin;
 
@@ -103,32 +105,69 @@ class StopwatchSettingTab extends PluginSettingTab {
 
 	display(): void {
 		let {containerEl} = this;
+		let p: HTMLElement = containerEl
 
 		containerEl.empty();
 
 		containerEl.createEl('h2', {text: 'Settings for stopwatch plugin.'});
 
-		new Setting(containerEl)
-			.setName('Stopwatch refresh interval')
-			.setDesc('Valid value range is 1~1000')
-			.addText(text => text
-				.setPlaceholder('Stopwatch refresh interval')
-				.setValue(this.plugin.settings.interval.toString())
-				.onChange(async (value) => {
-					try {
-						const i = parseInt(value.trim(), 10);
-						if (1000 >= i && i > 0) {
-							this.plugin.settings.interval = i
-							if (this.plugin.view != null) {
-								this.plugin.view.resetInterval()
-							}
-						}
-					} catch (e) {
-					}
-					await this.plugin.saveSettings();
-				}));
+		this.createIntervalSetting(containerEl)
+		this.createFormatSetting(containerEl);
+	}
 
-		const formatSetting = new Setting(containerEl)
+	createIntervalSetting(containerEl: HTMLElement) {
+		const setting = new Setting(containerEl)
+				.setName('Stopwatch refresh interval')
+				.setDesc(SETTING_INTERVAL_DESC)
+				.addText(text => text
+						.setPlaceholder('Stopwatch refresh interval')
+						.setValue(this.plugin.settings.interval.toString())
+						.onChange(async (value) => {
+							try {
+								const i = this.parseIntervalValue(value.trim());
+								console.log(`interval set to ${i}`)
+								this.plugin.settings.interval = i
+								await this.plugin.saveSettings();
+								setting.descEl.textContent = SETTING_INTERVAL_DESC
+								if (this.plugin.view != null) {
+									this.plugin.view.resetInterval()
+								}
+							} catch (e) {
+								console.log(e)
+								this.showIntervalAlert(setting, e.toString())
+							}
+						}));
+	}
+
+	private parseIntervalValue(src: string) {
+		const value = src.trim()
+		if (!value.match(/^[0-9]+$/)) {
+			throw Error("Value should be an integer")
+		}
+		const i = parseInt(value, 10)
+		if (1000 >= i && i > 0) {
+			return i
+		} else {
+			throw Error("Interval value out of range: " + i)
+		}
+	}
+
+	private showIntervalAlert(setting: Setting, message: string) {
+		setting.descEl.empty()
+		const container = setting.descEl.createDiv()
+		const note = setting.descEl.createDiv()
+		note.setText(SETTING_INTERVAL_DESC)
+		const alert = setting.descEl.createDiv({
+			cls: 'settings-interval-alert'
+		})
+		alert.setText(message)
+		console.log(message)
+		container.appendChild(note)
+		container.appendChild(alert)
+	}
+
+	private createFormatSetting(containerEl: HTMLElement) {
+		const setting = new Setting(containerEl)
 				.setName('Time Format')
 				.addText(component => {
 					component.setValue(this.plugin.settings.format)
@@ -139,7 +178,7 @@ class StopwatchSettingTab extends PluginSettingTab {
 								await this.plugin.saveSettings();
 							})
 				});
-		formatSetting.descEl.innerHTML = "For more syntax, refer to " +
+		setting.descEl.innerHTML = "For more syntax, refer to " +
 				"<a href='https://github.com/jsmreese/moment-duration-format#template-string'>format reference</a>"
 	}
 }
@@ -156,10 +195,10 @@ class StopWatchView extends ItemView {
 	private model: StopwatchModel;
 	private interval: number;
 
-	constructor(leaf: WorkspaceLeaf, plugin: StopwatchPlugin, model: StopwatchModel) {
+	constructor(leaf: WorkspaceLeaf, plugin: StopwatchPlugin) {
 		super(leaf);
 		this.plugin = plugin;
-		this.model = model;
+		this.model = new StopwatchModel();
 	}
 
 	getDisplayText(): string {
